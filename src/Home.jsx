5168b100d37c903ebca68dd40e5516f342e50681
MrMan314 <44348373+MrMan314@users.noreply.github.com>
2024-10-07 16:24:22 -0400

    my bad

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,9 +7,6 @@ export default function Login() {
 	return (
 		<React.StrictMode>
 			<Heading level={1}>Among Us</Heading>
-			<SimpleParallax orientation="right" scale={1.7}>
-				<img src="/fa0.png"/>
-			</SimpleParallax>
 		</React.StrictMode>
 	);
 }
